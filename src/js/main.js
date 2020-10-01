55119011a03a3a932656462063ca75a7baf255c7
RazvanBugoi <razvanbugoi@gmail.com>
2020-10-01 14:10:51 +0100

    added 2 input fields in index.html

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-console.log('test')
+function sum(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return Math.max(a, b) - Math.min(a, b);
+}
+
+function multiply(a, b) {
+    return a * b;
+}
