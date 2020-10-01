fcec38ec62713061c36b104df58d2f3cbaf3d5f7
RazvanBugoi <razvanbugoi@gmail.com>
2020-10-01 17:26:14 +0100

    stored input buttons in variables in app.js for further use

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,8 @@ import { sum, substract, multiply } from './main.js'
 
 let sum = document.getElementById('sum');
 let substract = document.getElementById('substract');
-let multiply = document.getElementById('multiply')
+let multiply = document.getElementById('multiply');
+let no1 = document.getElementById('number-1');
+let no2 = document.getElementById('number-2')
+
+sum.onclick = sum()
