b79cf0d4690e16d62b0270f381c1536b40d24d3f
RazvanBugoi <razvanbugoi@gmail.com>
2020-10-02 21:23:06 +0100

    changed variable names in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,9 @@
 import { sum, substract, multiply } from './main.js'
 
-let sum = document.getElementById('sum');
-let substract = document.getElementById('substract');
-let multiply = document.getElementById('multiply');
+let sumResult = document.getElementById('sum');
+let substractResult = document.getElementById('substract');
+let multiplyResult = document.getElementById('multiply');
 let no1 = document.getElementById('number-1');
 let no2 = document.getElementById('number-2')
 
-sum.onclick = sum(no1.value, no2.value);
+sumResult.onclick = sum;
