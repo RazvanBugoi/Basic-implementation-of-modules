08a1a7a724caee505f019e588337b136bb239d4e
RazvanBugoi <razvanbugoi@gmail.com>
2020-10-03 19:55:28 +0100

    changed variable names and onclick function

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,11 @@ import { sum, substract, multiply } from './main.js'
 let sumResult = document.getElementById('sum');
 let substractResult = document.getElementById('substract');
 let multiplyResult = document.getElementById('multiply');
-let no1 = document.getElementById('number-1');
-let no2 = document.getElementById('number-2')
+let firstNumber = document.getElementById('number-1');
+let secondNumber = document.getElementById('number-2')
 
-sumResult.onclick = sum;
+sumResult.onclick = function sumNumbers() {
+    console.log(sum(no1.value))
+}
+
+console.log(firstNumber.value)
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 function sum(a, b) {
-    return a + b;
+    console.log(Number(a) + Number(b));
 }
 
 function substract(a, b) {
