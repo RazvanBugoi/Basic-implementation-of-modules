33921a017dbaddf0067a84d95d4e8cd7ee277d49
RazvanBugoi <razvanbugoi@gmail.com>
2020-10-04 19:17:11 +0100

    renamed variables in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import { sum, difference, multiply } from './main.js'
 
 let sumResult = document.getElementById('sum');
-let substractResult = document.getElementById('substract');
+let differenceResult = document.getElementById('substract');
 let multiplyResult = document.getElementById('multiply');
 let firstNumber = document.getElementById('number-1');
 let secondNumber = document.getElementById('number-2')
@@ -11,7 +11,7 @@ sumResult.onclick = function sumNumbers() {
     document.getElementById('sum-result').innerText = `The result is ${result}`;
 }
 
-substractResult.onclick = function findTheDifference() {
+differenceResult.onclick = function findTheDifference() {
     let result = difference(firstNumber.value, secondNumber.value);
     document.getElementById('substract-result').innerText = `The result is ${result}`;
 }
